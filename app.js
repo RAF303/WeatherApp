5807fef344a591689e8071da283a2827a55eb5d9
Rafael Rodriguez <rafaelrodriguez@Rafaels-MacBook-Pro.local>
2020-02-13 19:13:55 -0700

    fully working

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 window.addEventListener('load', () => {
 	let long;
 	let lat;
+	let temperatureDescription = document.querySelector(
+		'.temperature-description'
+	);
+	let temperatureDegree = document.querySelector('.temperature-degree');
+	let loactionTimezone = document.querySelector('.location-timezone');
+	let temperatureSection = document.querySelector('.temperature');
+	const temperatureSpan = document.querySelector('.temperature span');
 
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(position => {
@@ -15,8 +22,34 @@ window.addEventListener('load', () => {
 					return response.json();
 				})
 				.then(data => {
-					console.log(data);
+					const { temperature, summary, icon } = data.currently;
+					// set DOM elements from API
+					temperatureDegree.textContent = temperature;
+					temperatureDescription.textContent = summary;
+					loactionTimezone.textContent = data.timezone;
+					//formula for celsius
+					let celsius = (temperature - 32) * (5 / 9);
+					// set icon
+					setIcons(icon, document.querySelector('.icon'));
+
+					//set temp to celsius
+					temperatureSection.addEventListener('click', () => {
+						if (temperatureSpan.textContent === 'F') {
+							temperatureSpan.textContent = 'C';
+							temperatureDegree.textContent = Math.floor(celsius);
+						} else {
+							temperatureSpan.textContent = 'F';
+							temperatureDegree.textContent = temperature;
+						}
+					});
 				});
 		});
+	}
+
+	function setIcons(icon, iconID) {
+		const skycons = new Skycons({ color: 'black' });
+		const currentIcon = icon.replace(/-/g, '_').toUpperCase();
+		skycons.play();
+		return skycons.set(iconID, Skycons[currentIcon]);
 	}
 });
